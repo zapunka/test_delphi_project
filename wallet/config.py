b8292cbf41a86ae8bd7e74b9441a2147b33d742f
polinka_dikaya <maksimovapi@gmail.com>
2021-05-19 11:41:22 +0300

    add user agent

--- wallet/config.py
+++ wallet/config.py
@@ -1,3 +1,14 @@
 TEST_LOGIN = 'tentthytime1985@sochi.com'
 TEST_PASSWORD = 'Korceze1994'
 ANTICAPTCHA_KEY = "7c0f71629683d84b413abba785654455"
+USER_AGENTS = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, как Gecko) Chrome/80.0.3987.163 Safari/537.36 OPR/67.0.3575.137',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, как Gecko) Chrome/80.0.3987.149 Safari/537.36 OPR/67.0.3575.115',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Версия/13.1 Safari/605.1.15',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36',
+]
--- wallet/main.py
+++ wallet/main.py
@@ -14,51 +14,43 @@ import wallet.config as conf
 
 
 def main():
-    # При каждой итерации меняем USERAGENT
-    U_A_File = open('useragent.txt')
-    U_A_File = U_A_File.readlines()
-    for line in U_A_File:
-        # anti-captcha plugin
-        options = webdriver.ChromeOptions()
-        options.add_extension('../wallet/anticaptcha-plugin_v0.54.zip')
+    options = webdriver.ChromeOptions()
+    options.add_extension('../wallet/anticaptcha-plugin_v0.54.crx')
+    options.add_argument("user-agent={}".format(get_random_user_agent()))
 
-        driver = webdriver.Chrome('../wallet/drivers/chromedriver')
-        if platform.system() == 'Linux':
-            driver = webdriver.Chrome("../wallet/drivers/chromedriver_linux", options=options)
-        actions = ActionChains(driver)
+    driver = webdriver.Chrome(executable_path='../wallet/drivers/chromedriver', options=options)
+    if platform.system() == 'Linux':
+        driver = webdriver.Chrome(executable_path="../wallet/drivers/chromedriver_linux", options=options)
+    actions = ActionChains(driver)
 
-        # Рандомно изменяем размер запускаемого окна
-        # sx = random.randint(1000, 1500)
-        # sn = random.randint(3000, 4500)
-        # driver.set_window_size(sx, sn)
-        #  END Рандомно изменяем размер запускаемого окна
+    # Рандомно изменяем размер запускаемого окна
+    # sx = random.randint(1000, 1500)
+    # sn = random.randint(3000, 4500)
+    # driver.set_window_size(sx, sn)
+    #  END Рандомно изменяем размер запускаемого окна
 
-        driver.get("https://play.alienworlds.io/")
-        time.sleep(20)
+    driver.get("https://play.alienworlds.io/")
+    time.sleep(40)
+    # WebDriverWait(driver, 120).until(lambda x: x.find_element_by_css_selector('.webgl-content'))
+    try:
         actions.move_to_element_with_offset(driver.find_element_by_tag_name('body'), 0, 0).move_by_offset(0, 160).click().perform()
         time.sleep(10)
         driver.switch_to.window(driver.window_handles[-2])
         time.sleep(5)
-
-        # Получаем элементы ввода данных через XPATH
-        login_input_path = '//*[@id="root"]/div/div/div[2]/div[5]/div/div/div/div[1]/div[1]/input'
-        driver.find_element_by_xpath(login_input_path).send_keys(conf.TEST_LOGIN)
-        pass_input_path = '//*[@id="root"]/div/div/div[2]/div[5]/div/div/div/div[1]/div[2]/input'
-        driver.find_element_by_xpath(pass_input_path).send_keys(conf.TEST_PASSWORD)
-        # Ожидание разгадывания капчи
-        WebDriverWait(driver, 120).until(lambda x: x.find_element_by_css_selector('.antigate_solver.solved'))
-        driver.find_element_by_css_selector('input[type=submit]').click()
-
-        # driver.find_element_by_xpath('//*[@id="root"]/div/div/div[2]/div[5]/div/div/div/div[4]/div/div/div/div/div/div/iframe').click()
-        # driver.find_element_by_xpath('//*[@id="root"]/div/div/div[2]/div[5]/div/div/div/div[5]/button[1]').click()
-        # END Получаем элементы ввода данных через XPATH
-        # АНТИКАПЧА
-        # custom_result = CustomResultHandler.CustomResultHandler(anticaptcha_key=ANTICAPTCHA_KEY)
-        # user_answer = custom_result.task_handler(task_id=TASK_ID)
-        # print(user_answer)
-
-
+    except IndexError:
+        print('Не смогли выбрать окно (возможно, не дождались его открытия)')
         driver.close()
+
+    # Получаем элементы ввода данных через XPATH
+    login_input_path = '//*[@id="root"]/div/div/div[2]/div[5]/div/div/div/div[1]/div[1]/input'
+    driver.find_element_by_xpath(login_input_path).send_keys(conf.TEST_LOGIN)
+    pass_input_path = '//*[@id="root"]/div/div/div[2]/div[5]/div/div/div/div[1]/div[2]/input'
+    driver.find_element_by_xpath(pass_input_path).send_keys(conf.TEST_PASSWORD)
+    # Ожидание разгадывания капчи
+    WebDriverWait(driver, 120).until(lambda x: x.find_element_by_css_selector('.antigate_solver.solved'))
+    driver.find_element_by_css_selector('input[type=submit]').click()
+
+    driver.close()
 
 
 def acp_api_send_request(driver, message_type, data={}):
@@ -72,10 +64,40 @@ def acp_api_send_request(driver, message_type, data={}):
     }
     # run JS code in the web page context
     # preceicely we send a standard window.postMessage method
-    return driver.execute_script("""
-    return window.postMessage({});
-    """.format(json.dumps(message)))
+    return driver.execute_script("""return window.postMessage({});""".format(json.dumps(message)))
+
+
+def captcha_solver():
+    options = webdriver.ChromeOptions()
+    options.add_extension('../wallet/anticaptcha-plugin_v0.54.crx')
+
+    browser = webdriver.Chrome(executable_path='../wallet/drivers/chromedriver', options=options)
+    acp_api_send_request(
+        browser,
+        'setOptions',
+        {'options': {'antiCaptchaApiKey': conf.ANTICAPTCHA_KEY}}
+    )
+    # 3 seconds pause
+    time.sleep(3)
+
+    # Go to the test form with reCAPTCHA 2
+    browser.get('https://antcpt.com/rus/information/demo-form/recaptcha-2.html')
+
+    # Test input
+    browser.find_element_by_name('demo_text').send_keys('Test input')
+
+    # Most important part: we wait upto 120 seconds until the AntiCaptcha plugin indicator with antigate_solver class
+    # gets the solved class, which means that the captcha was successfully solved
+    WebDriverWait(browser, 120).until(lambda x: x.find_element_by_css_selector('.antigate_solver.solved'))
+
+    # Sending form
+    browser.find_element_by_css_selector('input[type=submit]').click()
+
+
+def get_random_user_agent():
+    rand = random.randint(0, len(conf.USER_AGENTS)-1)
+    return conf.USER_AGENTS[rand]
 
 
 main()
-
+# captcha_solver()
